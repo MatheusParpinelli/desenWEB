78e993a30d3f2f933e43e42d43fdf5381485e3ab
Matheus Parpinelli <111715476+MatheusParpinelli@users.noreply.github.com>
2023-05-05 20:36:24 -0300

    Add files via upload

---
+++ aula8/js/meuScript.js
@@ -0,0 +1,23 @@
+function displayRadioValue(){
+    let elemento = document.getElementsByName('gender');
+    for(let i = 0; i < elemento.length; i++){
+        if(elemento[i].checked){
+            document.getElementById('result').innerHTML = "Genero selecionado é: " + elemento[i].value;
+        }
+    }
+}
+
+function getCheckBox(){
+    let checkBoxes = document.querySelectorAll('input[type="checkbox"]:checked');
+    text = "";
+    for(let i=0; i < checkBoxes.length; i++){
+        text = text + ", " + checkBoxes[i].value;
+    }
+    document.getElementById("pri").innerHTML = text;
+}
+
+function getOption(){
+    let selectElement = document.querySelector('#select1');
+    output = selectElement.value;
+    document.querySelector('.output').textContent = output;
+}
